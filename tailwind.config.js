1049d89a26ea1d2934d9ca657984f8387981ea4f
Jokebox90 <dev.jvagnier@gmail.com>
2023-07-19 08:17:30 +0200

    Ajoute des nouvelles libs et met à jour l'environnement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,74 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui";
+
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: "Ultra",
+      },
+      colors: {
+        primary: {
+          50: "#ECF3F8",
+          100: "#D9E6F2",
+          200: "#B7D0E6",
+          300: "#92B7D9",
+          400: "#6C9FCB",
+          500: "#4988BF",
+          600: "#366C9B",
+          700: "#295275",
+          800: "#1C374F",
+          900: "#0D1A26",
+          950: "#070D13",
+        },
+        secondary: {
+          50: "#F7F8E2",
+          100: "#EFF1C6",
+          200: "#DEE189",
+          300: "#CED350",
+          400: "#ABAF2C",
+          500: "#70731D",
+          600: "#5B5E18",
+          700: "#444511",
+          800: "#2C2D0B",
+          900: "#181806",
+          950: "#0C0C03",
+        },
+        accent: {
+          50: "#FAF2EA",
+          100: "#F5E2D1",
+          200: "#EAC5A4",
+          300: "#E0AA7A",
+          400: "#D68D4D",
+          500: "#BF712C",
+          600: "#995A23",
+          700: "#74441B",
+          800: "#4B2C11",
+          900: "#251609",
+          950: "#150C05",
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        owner: {
+          primary: "#4988BF",
+          secondary: "#70731D",
+          accent: "#BF712C",
+          neutral: "#78716c",
+          "base-100": "#f5f5f4",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
+  },
 };
-
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import macrosPlugin from "vite-plugin-babel-macros";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [macrosPlugin(), react()],
+  plugins: [react()],
 });
