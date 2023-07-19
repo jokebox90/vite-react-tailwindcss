eb4d10fd79b756ce40bb3ac580d2b9ffd0d6e447
Jokebox90 <dev.jvagnier@gmail.com>
2023-07-19 08:18:39 +0200

    Adapte les composants pour des rojets spécifiques

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,96 @@
 // src/pages/Home.tsx
 
+import _ from "lodash-es";
+import { useState } from "react";
+import { Isotope, IsotopeItem } from "../components/Isotope";
+import PriceQuickViews from "../components/PriceQuickViews";
+import PriceToggleView from "../components/PriceToggleView";
+import PriceView from "../components/PriceView";
+import PriceViewGroup from "../components/PriceViewGroup";
+import Sheet from "../components/Sheet";
 import { useAppConfig } from "../composables/appConfig";
-import Logos from "../components/Logos";
-import Counter from "../components/Counter";
-import Card from "../components/Card";
+import useCardProvider from "../composables/cardProvider";
 
 export default function Home() {
   const { config } = useAppConfig();
+  const cardProvider = useCardProvider();
+
+  const [state, setState] = useState({
+    togglePrice: true,
+    isotopeFilter: "filter-partner",
+  });
+
+  const togglePriceQuickViews = () =>
+    setState({ ...state, togglePrice: !state.togglePrice });
+
+  const showPrice = (price: number, term?: string) => {
+    const priceTerms = {
+      monthly: state.togglePrice ? Math.round((price * 9) / 12) : price,
+      annualy: state.togglePrice ? price * 9 : price * 12,
+    };
+
+    return _.get(priceTerms, term || "monthly");
+  };
+
+  const handleIsotopeFilter = (filter: string) => () =>
+    setState((prevState) => ({
+      ...prevState,
+      isotopeFilter: `filter-${_.kebabCase(filter)}`,
+    }));
+
+  const filters = {
+    starter: "filter-starter filter-advance filter-partner",
+    advance: "filter-advance filter-partner",
+    partner: "filter-partner",
+  };
 
   return (
-    <>
-      <Logos />
+    <div className="flex flex-col px-4 lg:px-20 py-10 shadow-md border-2 border-stone-300 rounded-2xl bg-stone-100">
+      <PriceQuickViews
+        title="Forfaits"
+        comment="Engagement sur un mois ou sur une année et surtout à volonté"
+      >
+        <PriceToggleView
+          toggle={togglePriceQuickViews}
+          variants={["Par mois", "Par années"]}
+        />
 
-      <h1>{config.projectName}</h1>
+        <PriceViewGroup {...state}>
+          <PriceView
+            quote="Démarrage"
+            title={`${showPrice(120, "monthly")} €`}
+            comment={`Soit ${showPrice(120, "annualy")} € / an`}
+            className="price-view-accent"
+            onClick={handleIsotopeFilter("starter")}
+            {...state}
+          />
+          <PriceView
+            quote="Avancé"
+            title={`${showPrice(240, "monthly")} €`}
+            comment={`Soit ${showPrice(240, "annualy")} € / an`}
+            className="price-view-accent"
+            onClick={handleIsotopeFilter("advance")}
+            {...state}
+          />
+          <PriceView
+            quote="Démarrage"
+            title={`${showPrice(600, "monthly")} €`}
+            comment={`Soit ${showPrice(600, "annualy")} € / an`}
+            active
+            className="price-view-accent"
+            onClick={handleIsotopeFilter("Partner")}
+            {...state}
+          />
+        </PriceViewGroup>
+      </PriceQuickViews>
 
-      <Card>
-        <Counter />
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </Card>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Isotope filter={state.isotopeFilter} className="mt-16">
+        {_.map(cardProvider.findAll(), (card, index) => (
+          <IsotopeItem key={index} className={_.get(filters, card.category)}>
+            <Sheet icon={card.icon} heading={card.title} body={card.body} />
+          </IsotopeItem>
+        ))}
+      </Isotope>
+    </div>
   );
 }
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,62 +1,159 @@
 // src/Root.tsx
 
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useLayoutEffect, useState } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { IconName } from "@fortawesome/fontawesome-svg-core";
+import _ from "lodash-es";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import FooterGreetings from "../components/FooterGreetings";
+import FooterLink from "../components/FooterLink";
+import FooterLinks from "../components/FooterLinks";
+import Landing from "../components/Landing";
+import LandingDescription from "../components/LandingDescription";
+import LandingNextSection from "../components/LandingNextSection";
+import LandingPoster from "../components/LandingPoster";
+import LandingQuote from "../components/LandingQuote";
+import LandingSection from "../components/LandingSection";
+import LandingTitle from "../components/LandingTitle";
+import MobileDevice from "../components/MobileDevice";
+import Navbar from "../components/Navbar";
+import NavbarBrand from "../components/NavbarBrand";
+import NavbarItem from "../components/NavbarItem";
+import NavbarMenu from "../components/NavbarMenu";
+import SocialLink from "../components/SocialLink";
+import SocialLinkGroup from "../components/SocialLinkGroup";
+import StackedList from "../components/StackedList";
+import StackedListItem from "../components/StackedListItem";
+import useCheckListProvider from "../composables/checkListProvider";
 
-type AppPropsType = {
+type AppProps = {
   config: object;
 };
 
-function isDarkSystemDefault() {
+export default function Root(Props: AppProps) {
+  const checkList = useCheckListProvider();
   return (
-    localStorage?.theme === "dark" ||
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  );
-}
-
-export default function Root(Props: AppPropsType) {
-  const [darkMode, setDarkMode] = useState(isDarkSystemDefault());
-
-  const toggleDarkMode = () => {
-    setDarkMode((darkMode) => !darkMode);
-  };
-
-  useLayoutEffect(() => {
-    if (darkMode) {
-      localStorage.theme = "dark";
-      document.documentElement.classList.add("dark");
-    } else {
-      delete localStorage.theme;
-      document.documentElement.classList.remove("dark");
-    }
-  }, [darkMode]);
-
-  return (
-    <>
+    <div data-theme="owner" className="max-w-screen bg-stone-200">
       <header className="header dark:header-dark">
-        <nav className="nav">
-          <NavLink to="/" className="nav-link">
-            Home
-          </NavLink>
+        <Navbar>
+          <NavbarBrand>PetitBoutDeCloud</NavbarBrand>
+          <NavbarMenu>
+            <NavbarItem to={`/`}>Home</NavbarItem>
+          </NavbarMenu>
+        </Navbar>
 
-          <NavLink to="/about" className="nav-link">
-            About
-          </NavLink>
+        <Landing className="bg-gradient-to-r from-primary-500 via-primary-400 to-primary-700">
+          <LandingSection
+            className="flex-1"
+            innerClass="justify-start items-center"
+          >
+            <div className="md:w-11/12 xl:w-4/5 2xl:w-full">
+              <LandingTitle className="underline underline-offset-8 text-accent-300">
+                PetitBoutDeCloud
+              </LandingTitle>
 
-          <button onClick={() => toggleDarkMode()} className="nav-link">
-            {darkMode ? (
-              <FontAwesomeIcon icon={"fa-solid fa-sun" as IconProp} />
-            ) : (
-              <FontAwesomeIcon icon={"fa-solid fa-moon" as IconProp} />
-            )}
-          </button>
-        </nav>
+              <LandingQuote
+                className="text-stone-50"
+                truncate={{
+                  length: 100,
+                }}
+              >
+                L’agence Web consciente des petits détails qui font les grandes
+                différences
+              </LandingQuote>
+            </div>
+
+            <LandingDescription className="self-center flex items-center bg-stone-100 rounded-r-3xl">
+              Notre équipe est dédiée à la création de projets Web sur mesure
+              pour les artisans, les boutiques et les organismes à Paris et en
+              Ile-De-France. Nous avons conçu pour vous approche progressive et
+              consciencieuse qui prend en compte votre rythme et vos attentes.
+              De cette façon, nous pouvons développer le site internet et/ou
+              l’application mobile qui reflète votre vision et vos valeurs. En
+              nous confiant votre projet, vous travaillez avec des partenaires
+              qui comprennent vos attentes et s'engagent à créer un outil
+              numérique qui favorise votre croissance et votre visibilité en
+              ligne.
+            </LandingDescription>
+          </LandingSection>
+
+          <LandingPoster className="self-center">
+            <MobileDevice>
+              <StackedList>
+                {_.map(checkList.find(1)?.body, (item, index) => (
+                  <StackedListItem
+                    key={index}
+                    id={`item-${item.id}`}
+                    text={item.text}
+                    icon={["fas", item.icon as IconName]}
+                  />
+                ))}
+              </StackedList>
+            </MobileDevice>
+          </LandingPoster>
+          <LandingNextSection size="2x" />
+        </Landing>
       </header>
-      <main className="main">
+
+      <main className="main" id="main">
         <Outlet context={{ config: Props.config }} />
       </main>
-    </>
+
+      <Footer>
+        <FooterLinks title="Services">
+          {_.map(
+            [
+              { title: "Web & Mobile" },
+              { title: "Design" },
+              { title: "Marketing" },
+              { title: "Kit SEO" },
+            ],
+            (item, index) => {
+              return <FooterLink key={index}>{item.title}</FooterLink>;
+            }
+          )}
+        </FooterLinks>
+
+        <FooterLinks title="Agence">
+          {_.map(
+            [
+              { title: "A propos" },
+              { title: "Contact" },
+              { title: "Partenaires" },
+              { title: "Blog" },
+            ],
+            (item, index) => {
+              return <FooterLink key={index}>{item.title}</FooterLink>;
+            }
+          )}
+        </FooterLinks>
+
+        <FooterLinks title="Mentions légales">
+          {_.map(
+            [
+              { title: "Conditions d'utilisation" },
+              { title: "Politique de confidentialité" },
+              { title: "Gestion des cookies" },
+            ],
+            (item, index) => {
+              return <FooterLink key={index}>{item.title}</FooterLink>;
+            }
+          )}
+        </FooterLinks>
+
+        <FooterGreetings
+          title="PetitBoutDeCloud"
+          icon={["fas", "cloud-download"]}
+          content="we got aa million ways to get IT, choose one !"
+          notes="Jonathan Vagnier"
+        >
+          <SocialLinkGroup>
+            <SocialLink icon="facebook-f" styles="blue" />
+            <SocialLink icon="instagram" styles="pink" />
+            <SocialLink icon="linkedin" styles="cyan" />
+            <SocialLink icon="twitter" styles="sky" />
+          </SocialLinkGroup>
+        </FooterGreetings>
+      </Footer>
+    </div>
   );
 }
